02401ed89c1f19a7f3af53349bea9436674d7edf
Artemius <artemcka.jdanov@yandex.ru>
2021-02-18 22:27:55 +0700

    Final rewrite

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import Timer from "./components/Timer";
-
 
 export default function App(props) {
   const [isShowing, setIsShowing] = useState(false);
-  const [delay, setDelay] = useState('');
+  const [delay, setDelay] = useState("");
 
   return (
-      <div className="wrapper">
-        <div>
-            <h1>Лабораторная №1. Функциональные компоненты.</h1>
-          <input
-              type="text"
-              value={delay}
-              onChange={e => setDelay(e.target.value)} />
-        </div>
-        <div>
-          Показать таймер:
-          <input
-              type="checkbox"
-              value={isShowing}
-              onChange={e => setIsShowing(e.target.checked)} />
-        </div>
-        <div>
-          {isShowing && <Timer delay={delay} />}
-        </div>
+    <div className="wrapper">
+      <div>
+        <h1>Лабораторная №1. Функциональные компоненты.</h1>
+        <input
+          className="radius"
+          type="text"
+          value={delay}
+          onChange={(e) => setDelay(e.target.value)}
+        />
       </div>
-    );
- }
-
-
+      <div>
+        <h3>Запуск таймера:</h3>
+        <input
+          type="checkbox"
+          id="_checkbox"
+          value={isShowing}
+          onChange={(e) => setIsShowing(e.target.checked)}
+        />
+        <label for="_checkbox">
+          <div id="tick_mark"></div>
+        </label>
+      </div>
+      <div>{isShowing && <Timer delay={delay} />}</div>
+    </div>
+  );
+}
